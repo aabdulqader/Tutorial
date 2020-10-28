b894ef8b6c71f04cc1e56a4bd07638ac5e3cb82e
shimul-aq <shimul.official7@gmail.com>
2020-10-29 00:27:09 +0600

    messages

--- home/views.py
+++ home/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .models import Tutorial
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+
 
 
 
@@ -25,11 +27,14 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user =form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'{username}! Your account has been created succesfully')
             login(request, user)
+            messages.info(request, f'{username}! Your are now login')
             return redirect('home:home')
         else:
             for msg in form.error_messages:
-                return(form.error_messages[msg])
+                messages.error(request, f'{msg}:{form.error_messages[msg]}')
     context = {
         'form':form, 
     }
